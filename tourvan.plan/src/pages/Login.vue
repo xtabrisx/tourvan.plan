a20971a97a35497193b34716e980d5697ae4e4df
xtabrisx <alberto.tocados@gmail.com>
2023-06-30 08:27:56 +0200

    routing 1

---
+++ tourvan.plan/src/pages/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+
+            <h1>Login Page</h1>
+            <hr>
+            <form action="">
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" class="form-control">
+
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Password</label>
+                    <input type="password" name="password" class="form-control">
+
+                </div>
+
+                <div class="my-3">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
